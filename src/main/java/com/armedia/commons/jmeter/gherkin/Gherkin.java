03a2e50bc9940cff68ca6c8f59fc5a83b6b0ed0f
Diego Rivera <diego.rivera@armedia.com>
2020-05-08 07:49:01 -0600

    Fixed copyright notices

--- src/main/java/com/armedia/commons/jmeter/gherkin/Gherkin.java
+++ src/main/java/com/armedia/commons/jmeter/gherkin/Gherkin.java
@@ -4,24 +4,24 @@
  * %%
  * Copyright (C) 2020 Armedia, LLC
  * %%
- * This file is part of the ArkCase software.
- *
- * If the software was purchased under a paid ArkCase license, the terms of
+ * This file is part of the Armedia JMeter Gherkin Plugin software.
+ * 
+ * If the software was purchased under a paid Armedia JMeter Gherkin Plugin license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- *
- * ArkCase is free software: you can redistribute it and/or modify
+ * 
+ * Armedia JMeter Gherkin Plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
- * ArkCase is distributed in the hope that it will be useful,
+ * 
+ * Armedia JMeter Gherkin Plugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * along with Armedia JMeter Gherkin Plugin. If not, see <http://www.gnu.org/licenses/>.
  * #L%
  *******************************************************************************/
 package com.armedia.commons.jmeter.gherkin;
